562df3ab2c8372c58ffb95b8154bb3b9066c4fd2
Jordan Santoli <jds442@drexel.edu>
2023-03-28 12:22:38 -0400

    Added coroutine support and onclick event to button

--- app/src/main/java/com/example/inclassappjunk/MainActivity.kt
+++ app/src/main/java/com/example/inclassappjunk/MainActivity.kt
@@ -6,34 +6,42 @@ import android.os.Handler
 import android.os.Looper
 import android.os.Message
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 import androidx.lifecycle.Lifecycle.Event.downTo
-
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+lateinit var timerTextView: TextView
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        timerTextView = findViewById(R.id.timerTextView)
 
 
-        val timerTextView: TextView by lazy {
-            findViewById(R.id.timerTextView)
-        }
+        val scope = CoroutineScope(Job() + Dispatchers.Main)
 
-        val timerHandler = Handler(Looper.getMainLooper()) {
-            timerTextView.
-        }
+        val button: Button = findViewById(R.id.button)
 
-        Thread{
-            for (i in 100 > downTo > (1)){
-                Log.d("Countdown", i.toString)
-                Thread.sleep(1000)
-                val msg = Message.obtain() //correct way to create message obj, will be sent to main thread
-
-                msg.what = i
-
-                timerHandler.sendEmptyMessage(i)
+        button.setOnClickListener() {
+            scope.launch {
+                countdownTimer()
             }
-        }.start()
+        }
+
+    }
+
+    suspend fun countdownTimer(){
+        repeat(100) {
+            (100 - it).toString().run {
+                timerTextView.text = this
+                Log.d("Countdown", this)
+            }
+            Thread.sleep(1000)
+
+        }
     }
 }
